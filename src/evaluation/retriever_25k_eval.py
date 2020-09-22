0c85a34135c9e322b1d7517321270be08fd06d5b
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-09-22 15:11:13 +0200

    ready for sbert retriever

--- src/evaluation/retriever_25k_eval.py
+++ src/evaluation/retriever_25k_eval.py
@@ -2,11 +2,12 @@
 Evaluates the Retriever with different input parameters
 
 Usage:
-    retriever_perf_eval_25k.py <test_corpus_path> <knowledge_base_path> <retriever_type> [options]
+    retriever_perf_eval_25k.py <test_corpus_path> <knowledge_base_path> <result_file_path> <retriever_type> [options]
 
 Arguments:
-    <test_corpus_path>             A path of a 25k dataset corpus file (the 25k QA from la DILA)
-    <knowledge_base_path>          A path of a knowledge-base corpus folder (the SPF uni/multi-fiches)
+    <test_corpus_path>             The path of a 25k dataset corpus file (the 25k QA from la DILA)
+    <knowledge_base_path>          The path of a knowledge-base corpus folder (the SPF uni/multi-fiches)
+    <result_file_path>             The path of a results file where to store the run's perf results
     <retriever_type>               Type of retriever to use (sparse (bm25 as usual) or dense (embeddings, SBERT))
     --cores=<n> CORES       Number of cores to use [default: 1:int]
 '''
@@ -15,6 +16,7 @@ import json
 import logging
 import subprocess
 import time
+from pathlib import Path
 from random import seed
 from typing import Dict, List
 
@@ -74,19 +76,20 @@ def compute_retriever_precision(true_fiches, retrieved_fiches, weight_position=F
     """
     summed_precision = 0
     for fiche_idx, true_fiche_id in enumerate(true_fiches):
-        for doc_idx, doc in enumerate(retrieved_fiches):
+        for retrieved_doc_idx, doc in enumerate(retrieved_fiches):
             if true_fiche_id in doc:
                 if weight_position:
-                    summed_precision += (fiche_idx + 1) / (doc_idx + 1)
+                    summed_precision += ((fiche_idx + 1) / (fiche_idx + retrieved_doc_idx + 1))
                 else:
                     summed_precision += 1
                 break
-    if weight_position:
-        summed_precision = summed_precision / len(true_fiches)
     return summed_precision
 
 
-def main(test_corpus_path: str, knowledge_base_path: str, retriever_type: str):
+# compute_retriever_precision(["f1", "f2", "f3"], ["f3"], False)
+
+def main(test_corpus_path: str, knowledge_base_path: str,
+         result_file_path: str, retriever_type: str):
     def eval_plot_k_range(min_k: int, max_k: int, weight_position: bool):
         """
         Queries ES max_k - min_k times, saving at each step the results in a list. At the end plots the line
@@ -98,24 +101,38 @@ def main(test_corpus_path: str, knowledge_base_path: str, retriever_type: str):
         """
         import seaborn as sns
         import matplotlib.pyplot as plt
+        # col names
+
         results = []
+        corpus_name = knowledge_base_path.split('/')[-2]
         for k in tqdm(range(min_k, max_k + 1)):
             tqdm.write(f"Testing k={k}")
-            mean_precision_weighted, avg_time, detailed_results_weighted = compute_score(retriever=retriever,
-                                                                                         retriever_top_k=k,
-                                                                                         test_dataset=test_dataset,
-                                                                                         weight_position=True)
-            mean_precision, avg_time, detailed_results = compute_score(retriever=retriever, retriever_top_k=k,
-                                                                       test_dataset=test_dataset, weight_position=False)
-            results.append({"k": k, "mean_precision_weighted": mean_precision_weighted,
-                            "mean_precision": mean_precision})
-        df_results: pd.DataFrame = pd.DataFrame(results).set_index("k")
-        sns.lineplot(data=df_results[["mean_precision_weighted", "mean_precision"]])
-        fig_title = f"kb={knowledge_base_path.split('/')[-2]}--k={min_k},{max_k}--retriever={retriever_type}--weighted=both"
-        plt.title(fig_title)
+            mean_precision, avg_time, correctly_retrieved, detailed_results_weighted = compute_score(
+                retriever=retriever,
+                retriever_top_k=k,
+                test_dataset=test_dataset,
+                weight_position=weight_position)
+            result_dict = {"k": k,
+                           "corpus": corpus_name,
+                           "retriever": retriever_type,
+                           "nb_documents": len(test_dataset),
+                           "correctly_retrieved": correctly_retrieved,
+                           "weighted_precision": str(weight_position),
+                           "precision": mean_precision,
+                           "avg_time_s": avg_time}
 
-        plt.savefig(f"./results/{fig_title}.png")
-        df_results.to_csv(f"./results/{fig_title}.csv", index=False)
+            results.append(result_dict)
+        df_results: pd.DataFrame = pd.DataFrame(results)
+        # sns.lineplot(data=df_results[["mean_precision_weighted", "mean_precision"]])
+        fig_title = f"kb={corpus_name}--k={min_k},{max_k}--retriever={retriever_type}--weighted={weight_position}"
+        # plt.title(fig_title)
+        #
+        # plt.savefig(f"./results/{fig_title}.png")
+        if Path(result_file_path).exists():
+            df_old = pd.read_csv(result_file_path)
+            df_results = pd.concat([df_old, df_results])
+        with open(result_file_path, "w") as filo:
+            df_results.to_csv(filo, index=False)
 
     def single_run(retriever_top_k: int):
         """
@@ -123,8 +140,10 @@ def main(test_corpus_path: str, knowledge_base_path: str, retriever_type: str):
         :param retriever_top_k:
         :return:
         """
-        mean_precision, avg_time, detailed_results = compute_score(retriever=retriever, retriever_top_k=retriever_top_k,
-                                                                   test_dataset=test_dataset, weight_position=True)
+        mean_precision, avg_time, found_fiche, detailed_results = compute_score(retriever=retriever,
+                                                                                retriever_top_k=retriever_top_k,
+                                                                                test_dataset=test_dataset,
+                                                                                weight_position=True)
         with open(f"./results/k_{retriever_top_k}_detailed_results.json", "w") as outo:
             json.dump(detailed_results, outo, indent=4, ensure_ascii=False)
 
@@ -134,11 +153,8 @@ def main(test_corpus_path: str, knowledge_base_path: str, retriever_type: str):
         logger.info("Could not prepare the testing framework!! Exiting :(")
         return
 
-    single_run(retriever_top_k=3)
+    # single_run(retriever_top_k=3)
     eval_plot_k_range(1, 10, weight_position=True)
-
-
-LAUNCH_ELASTICSEARCH = True
 
 
 def prepare_framework(knowledge_base_path: str = "/data/service-public-france/extracted/",
@@ -170,12 +186,7 @@ def prepare_framework(knowledge_base_path: str = "/data/service-public-france/ex
         dicts = convert_json_files_to_dicts(dir_path=knowledge_base_path)
 
         # Now, let's write the docs to our DB.
-        if LAUNCH_ELASTICSEARCH:
-            document_store.write_documents(dicts)
-        else:
-            logger.warning(
-                "Since we already have a running ES instance we should not index the same documents again. \n"
-                "If you still want to do this call: document_store.write_documents(dicts) manually ")
+        document_store.write_documents(dicts)
 
         if retriever_type == "sparse":
             retriever = ElasticsearchRetriever(document_store=document_store)
@@ -232,13 +243,15 @@ def compute_score(retriever: BaseRetriever, retriever_top_k: int,
     tqdm.write(
         f"The retriever correctly found {found_fiche} fiches among {len(test_dataset)}. Mean_precision {mean_precision}. "
         f"Time per ES query (ms): {avg_time * 1000:.3f}")
-    return mean_precision, avg_time, detailed_results
+    return mean_precision, avg_time, found_fiche, detailed_results
 
 
 if __name__ == '__main__':
     parser = argopt(__doc__).parse_args()
     test_corpus_path = parser.test_corpus_path
     knowledge_base_path = parser.knowledge_base_path
+    result_file_path = parser.result_file_path
     retriever_type = parser.retriever_type
     # TODO: as as parameters the weighted_computation and k_range
-    main(test_corpus_path=test_corpus_path, knowledge_base_path=knowledge_base_path, retriever_type=retriever_type)
+    main(test_corpus_path=test_corpus_path, knowledge_base_path=knowledge_base_path,
+         result_file_path=result_file_path, retriever_type=retriever_type)
