d0c5fb2a6ea029b8f444e91a51228769ba3c0e3b
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-09-29 14:33:26 +0200

    :art: refactoring convert_jsonand_embeddings function

--- src/util/convert_json_to_dictsAndEmbeddings.py
+++ src/util/convert_json_to_dictsAndEmbeddings.py
@@ -1,4 +1,12 @@
-def convert_json_to_dictsAndEmbeddings(dir_path: str, split_paragraphs: bool = False):
+import json
+from pathlib import Path
+
+from haystack.retriever.dense import EmbeddingRetriever
+from tqdm import tqdm
+
+
+def convert_json_to_dictsAndEmbeddings(dir_path: str, retriever: EmbeddingRetriever,
+                                       split_paragraphs: bool = False):
     """
     Convert all Json in the sub-directories of the given path to Python dicts that can be written to a
     Document Store.
@@ -7,6 +15,7 @@ def convert_json_to_dictsAndEmbeddings(dir_path: str, split_paragraphs: bool = F
         text : string,
         link : string
     }
+    :param retriever:
     :param dir_path: path for the documents to be written to the database
     :param clean_func: a custom cleaning function that gets applied to each doc (input: str, output:str)
     :param split_paragraphs: split text in paragraphs.
@@ -16,7 +25,7 @@ def convert_json_to_dictsAndEmbeddings(dir_path: str, split_paragraphs: bool = F
     file_paths = [p for p in Path(dir_path).glob("**/*")]
 
     documents = []
-    for path in file_paths:
+    for path in tqdm(file_paths):
         if path.suffix.lower() == ".json":
             with open(path) as doc:
                 jsonDoc = json.load(doc)
@@ -25,10 +34,10 @@ def convert_json_to_dictsAndEmbeddings(dir_path: str, split_paragraphs: bool = F
         else:
             raise Exception(f"Indexing of {path.suffix} files is not currently supported.")
 
-
         if split_paragraphs:
             raise Exception(f"Splitting paragraph not currently supported.")
         else:
-            documents.append({"text": text, "question": text, "question_emb": question_emb, "meta": {"name": path.name, "link": f"https://www.service-public.fr/particuliers/vosdroits/{path.name.split('--', 1)[0]}" }})
+            documents.append({"text": text, "question": text, "question_emb": question_emb, "meta": {"name": path.name,
+                                                                                                     "link": f"https://www.service-public.fr/particuliers/vosdroits/{path.name.split('--', 1)[0]}"}})
 
     return documents
