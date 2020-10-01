9e43f5a7801b53f9f363e6f90ac68014c3ae6ba1
Rob192 <reynaud.robin@gmail.com>
2020-10-01 14:29:57 +0200

    last version of retriever_25k_eval_theme.py for merging

--- src/evaluation/retriever_25k_eval_theme.py
+++ src/evaluation/retriever_25k_eval_theme.py
@@ -1,4 +1,4 @@
-'''
+"""
 Evaluates the Retriever with different input parameters
 
 Usage:
@@ -11,7 +11,7 @@ Arguments:
     <retriever_type>               Type of retriever to use (sparse (bm25 as usual) or dense (embeddings, SBERT))
     <filtered>
     --cores=<n> CORES       Number of cores to use [default: 1:int]
-'''
+"""
 
 import json
 import logging
@@ -42,40 +42,38 @@ logger = logging.getLogger(__name__)
 # MODEL_TOKENIZER = "etalab-ia/camembert-base-squadFR-fquad-piaf"
 # RETRIEVER = "sparse"  # sparse = bm25 ; dense = embeddings
 
-def get_filters_list(test_corpus_path):
-    df = pd.read_csv(test_corpus_path).fillna("")
-    theme_list = list(df.theme.unique())
-    dossier_list = list(df.dossier.unique())
-    return  theme_list, dossier_list
-
-def load_25k_test_set(test_corpus_path: str, filter):
+def load_25k_test_set(corpus_path: str):
     """
     Loads the 25k dataset. The 25k dataset is a csv that must contain the url columns (url, url2, url3, url4)
     and a question column. The former contains the list of proposed fiches' URLs and the latter contains the
     question sent by an user.
-    :param test_corpus_path: Path of the file containing the 25k corpus
-    :return: Dict with the questions as key and the referenced fiches' URLs as values, List of Themes, List of Dossiers
+    :param corpus_path: Path of the file containing the 25k corpus
+    :return: Dict with the questions as key and a meta dict as values,
+    the meta dict containing urls where the answer is and the arborescence of where the answer lies
     """
     url_cols = ["url", "url_2", "url_3", "url_4"]
 
-    df = pd.read_csv(test_corpus_path).fillna("")
+    df = pd.read_csv(corpus_path).fillna("")
     dict_question_fiche = {}
     for row in df.iterrows():
-        if row[1]["theme"] == filter:
-            question = row[1]["question"]
-            list_url = [row[1][u] for u in url_cols if row[1][u]]
-            dict_question_fiche[question] = list_url
+        question = row[1]["question"]
+        list_url = [row[1][u] for u in url_cols if row[1][u]]
+        arbo = {'theme': row[1]['theme'],
+                'dossier': row[1]['dossier']}
+        meta = {'urls': list_url, 'arbo': arbo}
+        dict_question_fiche[question] = meta
+
     return dict_question_fiche
 
 
-def compute_retriever_precision(true_fiches, retrieved_fiches, weight_position=False):
+def compute_retriever_precision(true_fiches, retrieved_fiches, weight_position):
     """
     Computes an accuracy-like score to determine the fairness of the retriever.
     Takes the k *retrieved* fiches' names and counts how many of them exist in the *true* fiches names
 
-
     :param retrieved_fiches:
     :param true_fiches:
+    :param weight_position: Bool indicates if the precision must be calculated with a weighted precision
     :return:
     """
     summed_precision = 0
@@ -92,29 +90,29 @@ def compute_retriever_precision(true_fiches, retrieved_fiches, weight_position=F
 
 # compute_retriever_precision(["f1", "f2", "f3"], ["f3"], False)
 
-def main(test_corpus_path: str, knowledge_base_path: str,
-         result_file_path: str, retriever_type: str):
+def main(test_corpus_path: str, knowledge_base_path: str, result_file_path: str, retriever_type: str, filter_level: str,
+         k_value: int = 1, weight_position: bool = True):
 
     def eval_plot_k(k: int, weight_position: bool):
         """
         Queries ES max_k - min_k times, saving at each step the results in a list. At the end plots the line
         showing the results obtained. For now we can only vary k.
-        :param min_k: Minimum retriever-k to test
-        :param max_k: Maximum retriever-k to test
+        :param k: Number of retriever-k to test
         :param weight_position: Whether to take into account the position of the retrieved result in the accuracy computation
         :return:
         """
         corpus_name = knowledge_base_path.split('/')[-2]
         tqdm.write(f"Testing k={k}")
-        mean_precision, avg_time, correctly_retrieved, detailed_results_weighted = compute_score(
-            retriever=retriever,
-            retriever_top_k=k,
-            test_dataset=test_dataset,
-            weight_position=weight_position)
+        mean_precision, avg_time, correctly_retrieved, detailed_results_weighted, nb_questions = compute_score(retriever=retriever,
+                                                                                                 retriever_top_k=k,
+                                                                                                 test_dataset=test_dataset,
+                                                                                                 weight_position=weight_position,
+                                                                                                 filter_level=filter_level)
         result_dict = {"k": k,
                        "corpus": corpus_name,
                        "retriever": retriever_type,
-                       "nb_documents": len(test_dataset),
+                       "filter_level": filter_level,
+                       "nb_documents": nb_questions,
                        "correctly_retrieved": correctly_retrieved,
                        "weighted_precision": str(weight_position),
                        "precision": mean_precision,
@@ -127,28 +125,23 @@ def main(test_corpus_path: str, knowledge_base_path: str,
         :param retriever_top_k:
         :return:
         """
-        mean_precision, avg_time, found_fiche, detailed_results = compute_score(retriever=retriever,
+        mean_precision, avg_time, found_fiche, detailed_results, nb_questions = compute_score(retriever=retriever,
                                                                                 retriever_top_k=retriever_top_k,
                                                                                 test_dataset=test_dataset,
                                                                                 weight_position=True)
         with open(f"./results/k_{retriever_top_k}_detailed_results.json", "w") as outo:
             json.dump(detailed_results, outo, indent=4, ensure_ascii=False)
 
-    theme_list, dossier_list = get_filters_list(test_corpus_path)
+    retriever = prepare_framework(knowledge_base_path=knowledge_base_path, retriever_type=retriever_type)
+    if not retriever:
+        logger.info("Could not prepare the testing framework!! Exiting :(")
+        return
 
     results = []
-    for theme in theme_list:
-        test_dataset = load_25k_test_set(test_corpus_path, theme)
-        retriever = prepare_framework(knowledge_base_path=knowledge_base_path, retriever_type=retriever_type, filter_name=theme)
-        if not retriever:
-            logger.info("Could not prepare the testing framework!! Exiting :(")
-            continue
 
-        for k in range (1, 2):
-            # single_run(retriever_top_k=3)
-            result_dict = eval_plot_k(k, weight_position=False)
-            result_dict['filter_name'] = theme
-            results.append(result_dict)
+    test_dataset = load_25k_test_set(test_corpus_path)
+    result_dict = eval_plot_k(k_value, weight_position=weight_position)
+    results.append(result_dict)
     df_results: pd.DataFrame = pd.DataFrame(results)
     if Path(result_file_path).exists():
         df_old = pd.read_csv(result_file_path)
@@ -158,23 +151,13 @@ def main(test_corpus_path: str, knowledge_base_path: str,
 
 
 def prepare_framework(knowledge_base_path: str = "/data/service-public-france/extracted/",
-                      retriever_type: str = "sparse",
-                      filter_name: str = "Famille"):
+                      retriever_type: str = "sparse"):
     """
     Loads ES if needed (check LAUNCH_ES var above) and indexes the knowledge_base corpus
     :param knowledge_base_path: PAth of the folder containing the knowledge_base corpus
     :param retriever_type: The type of retriever to be used
     :return: A Retriever object ready to be queried
     """
-    def filter_dicts(dicts, filter):
-        result = []
-        for dic in dicts:
-            try:
-                if dic['meta']['arborescence']['theme'] == filter:
-                    result.append(dic)
-            except:
-                continue
-        return result
 
     try:
         # kill ES container if running
@@ -196,8 +179,6 @@ def prepare_framework(knowledge_base_path: str = "/data/service-public-france/ex
 
         dicts = convert_json_files_v10_to_dicts(dir_path=knowledge_base_path)
 
-        dicts = filter_dicts(dicts, filter_name)
-
         # Now, let's write the docs to our DB
         document_store.write_documents(dicts)
 
@@ -211,8 +192,8 @@ def prepare_framework(knowledge_base_path: str = "/data/service-public-france/ex
         logger.error(f"Failed with error {str(e)}")
 
 
-def compute_score(retriever: BaseRetriever, retriever_top_k: int,
-                  test_dataset: Dict[str, List], weight_position: bool = False):
+def compute_score(retriever: BaseRetriever, retriever_top_k: int, test_dataset,
+                  weight_position: bool = False, filter_level: str = None):
     """
     Given a Retriever to query and its parameters and a test dataset (couple query->true related doc), computes
     the number of matches found by the Retriever. A match is succesful if the retrieved document is among the
@@ -221,22 +202,33 @@ def compute_score(retriever: BaseRetriever, retriever_top_k: int,
     :param retriever_top_k: The number of docs to retrieve
     :param test_dataset: A collection of "query":[relevant_doc_1, relevant_doc_2, ...]
     :param weight_position: Whether to take into account the position of the retrieved result in the accuracy computation
+    :param filter_level: The name of the filter requested, usually the level from the arborescence : 'theme', 'dossier' ..
     :return: Returns mean_precision, avg_time, and detailed_results
     """
     summed_precision = 0
     found_fiche = 0
+    nb_questions = 0
     succeses = []
     errors = []
     if weight_position:
         logger.info("Using position weighted accuracy")
     pbar = tqdm(total=len(test_dataset))
-    for question, true_fiche_urls in test_dataset.items():
+    for question, meta in test_dataset.items():
+        arborescence = meta['arbo']
+        filter_value = arborescence[filter_level]
+        if filter_level is not None and filter_value == '': #sometimes the value for the filter is not present in the data
+            continue
+        true_fiche_urls = meta['urls']
         true_fiche_ids = [f.split("/")[-1] for f in true_fiche_urls]
-        retrieved_results = retriever.retrieve(query=question, top_k=retriever_top_k)
+        if filter_level == None:
+            retrieved_results = retriever.retrieve(query=question, top_k=retriever_top_k)
+        else:
+            retrieved_results = retriever.retrieve(query=question, filters={filter_level: [filter_value]}, top_k=retriever_top_k)
         pbar.update()
         retrieved_doc_names = [f.meta["name"] for f in retrieved_results]
         precision = compute_retriever_precision(true_fiche_ids, retrieved_doc_names, weight_position=weight_position)
         summed_precision += precision
+        nb_questions += 1
         if precision:
             found_fiche += 1
             succeses.append({"question": question,
@@ -250,14 +242,16 @@ def compute_score(retriever: BaseRetriever, retriever_top_k: int,
                            })
 
     avg_time = pbar.avg_time
+    if avg_time is None: #quick fix for a bug idk why is happening
+        avg_time = 0
     pbar.close()
     detailed_results = {"successes": succeses, "errors": errors, "avg_time": avg_time}
 
-    mean_precision = summed_precision / len(test_dataset)
+    mean_precision = summed_precision / nb_questions
     tqdm.write(
-        f"The retriever correctly found {found_fiche} fiches among {len(test_dataset)}. Mean_precision {mean_precision}. "
+        f"The retriever correctly found {found_fiche} fiches among {nb_questions}. Mean_precision {mean_precision}. "
         f"Time per ES query (ms): {avg_time * 1000:.3f}")
-    return mean_precision, avg_time, found_fiche, detailed_results
+    return mean_precision, avg_time, found_fiche, detailed_results, nb_questions
 
 
 if __name__ == '__main__':
@@ -266,6 +260,10 @@ if __name__ == '__main__':
     knowledge_base_path = parser.knowledge_base_path
     result_file_path = parser.result_file_path
     retriever_type = parser.retriever_type
-    # TODO: as as parameters the weighted_computation and k_range
-    main(test_corpus_path=test_corpus_path, knowledge_base_path=knowledge_base_path,
-         result_file_path=result_file_path, retriever_type=retriever_type)
+
+    weight_position = True
+    for filter_level in [None, 'theme', 'dossier']:
+        for k in range(3,6,2):
+            main(test_corpus_path=test_corpus_path, knowledge_base_path=knowledge_base_path,
+                 result_file_path=result_file_path, retriever_type=retriever_type, filter_level=filter_level, k_value=k,
+                 weight_position=weight_position)
