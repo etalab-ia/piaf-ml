ca019e8d67d31830604ee468e1d05b37866d8b00
Giulia Santarsieri <giulia.santarsieri@data.gouv.fr>
2020-10-02 15:24:50 +0200

    fix bug dataset

--- src/data/split_train_test.py
+++ src/data/split_train_test.py
@@ -5,6 +5,8 @@ To use it, pass a percentage of the desired size for the train
 
 from pathlib import Path
 import sys
+import os
+print(os.getcwd())
 
 if len(sys.argv) < 2:
     print("Please indicate the train %. Exiting...")
@@ -12,13 +14,13 @@ if len(sys.argv) < 2:
 
 TRAIN_PCT = float(sys.argv[1])
 
-path_file = Path("all_fiches.txt")
+path_file = Path("data/all_fiches_raw.txt")
 file_size = path_file.stat().st_size
 train_size = int(file_size * TRAIN_PCT)
-with open("all_fiches.txt") as fiches:
+with open(path_file) as fiches:
     train_content = fiches.read(train_size)
     test_content = fiches.read()
-with open("train_fiches.txt", "w") as out:
+with open("data/train_fiches.txt", "w") as out:
     out.write(train_content)
-with open("test_fiches.txt", "w") as out:
+with open("data/test_fiches.txt", "w") as out:
     out.write(test_content)
