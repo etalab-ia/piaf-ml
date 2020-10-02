e6b5a717bcf1b2fad8848dd82f39718fc2376045
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-10-02 16:13:11 +0200

    :bug fixed single line text in mail corpus

--- src/data/mails_csv_txt.py
+++ src/data/mails_csv_txt.py
@@ -2,9 +2,12 @@
 This code converts all the .csv files "export*.csv" into a whole new mail.txt file
 """
 import pandas as pd
+from tqdm import tqdm
 
-mail = pd.read_csv('data/1_an_historique/mail.csv',sep=';',low_memory=False)
-mail_list=mail['Corps du mail'].tolist()
-a='.'.join(map(str,mail_list))
-with open("data/1_an_historique/mail.txt", "w") as text_file:
-    text_file.write(str(mail_list))
+mail = pd.read_csv('data/1_an_historique/mail.csv', sep=';', low_memory=False)
+mail_list = mail['Corps du mail'].tolist()
+mail_list = list(map(str, mail_list))
+with open("data/1_an_historique/corpus_mail_spf.txt", "w") as text_file:
+    for line in tqdm(mail_list):
+        text_file.write(line)
+        text_file.write("\n")
