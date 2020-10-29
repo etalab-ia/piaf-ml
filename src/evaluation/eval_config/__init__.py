77dce9852a7af19019645b292daf398ad4122fae
Rob192 <reynaud.robin@gmail.com>
2020-10-29 09:23:50 +0100

    add experimentation for learning correctly retrieved fiche from classifier

--- src/evaluation/eval_config/__init__.py
+++ src/evaluation/eval_config/__init__.py
@@ -1,11 +1,11 @@
 parameters = {
-    "k": [1],
+    "k": [30,100],
     "retriever_type": ["sparse",'dense'],
     "knowledge_base": ["./data/v11"],
                        # "/home/pavel/code/piaf-ml/data/v10"],
     "test_dataset": ["./data/407_question-fiche_anonym.csv"],
     "weighted_precision": [True],
-    "filter_level": [None, 'theme', 'dossier'],
+    "filter_level": ['theme','dossier'],
     "lemma_preprocessing": [False, True]
 }
 # rules:
