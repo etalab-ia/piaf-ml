9a66552cb370cd4593779829b0568a8655a9198b
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-11-12 11:07:09 +0100

    fixed try/catch block while loading a retriever

--- src/evaluation/retriever_25k_eval.py
+++ src/evaluation/retriever_25k_eval.py
@@ -166,8 +166,7 @@ def single_run(parameters):
                                preprocessing=lemma_preprocessing)
 
     if not retriever:
-        logger.info("Could not prepare the testing framework!! Exiting :(")
-        return
+        raise Exception(("Could not prepare the testing framework!! Exiting :("))
 
     clean_function = None
     if lemma_preprocessing:
@@ -338,11 +337,10 @@ def load_retriever(knowledge_base_path: str = "/data/service-public-france/extra
                                       preprocessing=preprocessing)
 
             document_store.write_documents(dicts)
-
+            return retriever
     except Exception as e:
         logger.error(f"Failed with error {str(e)}")
-    finally:
-        return retriever
+        return
 
 
 def compute_score(retriever: BaseRetriever, retriever_top_k: int,
