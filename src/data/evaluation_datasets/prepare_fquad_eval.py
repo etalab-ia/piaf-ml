24a085fdef1a65d1b7be6016534b8b4514f87f4d
Rob192 <reynaud.robin@gmail.com>
2021-01-20 19:15:28 +0100

    code cleaning for prepare_fquad_eval.py

--- src/data/evaluation_datasets/prepare_fquad_eval.py
+++ src/data/evaluation_datasets/prepare_fquad_eval.py
@@ -1,70 +1,19 @@
 from pathlib import Path
 import json
-import pandas as pd
 
 
-def prepare_test_set(squad_path: str):
-    """
-    Loads the squad dataset.
-    :param corpus_path: Path of the file containing the squad dataset
-    :return: List of questions with the questions given in the squad form + the context
-    [
-        {
-            id: "Id du pair question-réponse"
-            question: "Question"
-            context: "Paragraphe de l'article"
-            title: "Titre de l'article Wikipedia"
-            answers:[
-                {
-                    "answer_start": "Position de la réponse"
-                    "text": "Réponse"
-                }
-            ]
-        }
-    ]
-    """
-
-    with open(squad_path, encoding='UTF-8') as f:
-        fquad = json.load(f)
-
-    list_questions = []
-    list_articles = fquad['data']
-    for article in list_articles:
-        title = article['title']
-        for paragraph in article['paragraphs']:
-            context = paragraph['context']
-            for question in paragraph['qas']:
-                question['title'] = title
-                question['context'] = context
-                list_questions.append(question)
-    return list_questions
-
-def split_test_set (file):
-    with open(file, encoding='UTF-8') as f:
-        fquad = json.load(f)
-    fquad['data'] = [fquad['data'][0]]
-    res_file = file.parent / (file.stem +'_fraction'+ file.suffix)
-
-    with open(res_file, 'w', encoding='UTF-8') as f:
-        json.dump(fquad,f)
-
-def add_is_impossible (fquad):
-    # with open(file, encoding='UTF-8') as f:
-    #     fquad = json.load(f)
-
+def add_is_impossible(fquad):
+    """ this function adds the key 'is_impossible' to a fquad formated dictionnary in order to go from squad v1 format to squad v2 format"""
     for question in fquad['data']:
         for paragraph in question['paragraphs']:
             for qas in paragraph['qas']:
                 qas['is_impossible'] = False
-
-    # res_file = file.parent / (file.stem + '_with_impossible' + file.suffix)
-    #
-    # with open(res_file, 'w', encoding='UTF-8') as f:
-    #     json.dump(fquad,f)
-
     return fquad
 
+
 def remove_answers_from_kb(fquad):
+    """ this function is used to remove the answers from the fquad formated dictionnary in order to use them as a
+    knowledge base"""
     for article in fquad['data']:
         for paragraph in article['paragraphs']:
             for qa in paragraph['qas']:
@@ -72,7 +21,10 @@ def remove_answers_from_kb(fquad):
                 qa['is_impossible'] = True
     return fquad
 
+
 def merge(kb_fquad, test_fquad):
+    """ this function is used for merging two dataset together. One is the knowledge base dataset, without the answers,
+     and the other is the dataset that will be used for evaluation of the haystack pipeline"""
     modified_fquad = kb_fquad.copy()
     existing_titles = [article['title'] for article in kb_fquad['data']]
     for article in test_fquad['data']:
@@ -82,27 +34,21 @@ def merge(kb_fquad, test_fquad):
             for paragraph in article['paragraphs']:
                 if paragraph['context'] in existing_contexts:
                     index_paragraph = existing_contexts.index(paragraph['context'])
-                    existing_questions = [qas['question'] for qas in kb_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas']]
+                    existing_questions = [qas['question'] for qas in
+                                          kb_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas']]
                     for qas in paragraph['qas']:
                         if qas['question'] in existing_questions:
                             index_qas = existing_questions.index(qas['question'])
-                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['answers'] = qas['answers']
-                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['is_impossible'] = False
-                            # for answer in qas['answers']:
-                            #     existing_texts = [answer['text'] for answer in kb_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['answers']]
-                            #     if answer['text'] not in existing_texts:
-                            #         existing_answers = modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['answers']
-                            #         existing_answers.append(answer)
-                            #         # if an answer is added, remove the empty answers from the knowledge base
-                            #         for existing_answer in existing_answers:
-                            #             if existing_answer['text'] == "":
-                            #                 existing_answers.remove(existing_answer)
-                            #         modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['answers'] = existing_answers
-                            #         modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]['is_impossible'] = False
+                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas][
+                                'answers'] = qas['answers']
+                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas][
+                                'is_impossible'] = False
                         else:
-                            existing_qas = modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas]
+                            existing_qas = modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][
+                                index_qas]
                             existing_qas.append(qas)
-                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][index_qas] = existing_qas
+                            modified_fquad['data'][index_title]['paragraphs'][index_paragraph]['qas'][
+                                index_qas] = existing_qas
                 else:
                     existing_paragraphs = modified_fquad['data'][index_title]['paragraphs']
                     existing_paragraphs.append(paragraph)
@@ -114,14 +60,15 @@ def merge(kb_fquad, test_fquad):
 
     return modified_fquad
 
+
 def save_dataset(modified_fquad, name='fquad_eval'):
+    """ save the dataset in the /data/evaluation-datasets folder"""
     res_file = Path('./data/evaluation-datasets') / (name + '.json')
     with open(res_file, 'w', encoding='UTF-8') as f:
-        json.dump(modified_fquad,f)
+        json.dump(modified_fquad, f)
 
 
-
-def main(file_kb_fquad,file_test_fquad, name='fquad_eval'):
+def main(file_kb_fquad, file_test_fquad, name='fquad_eval'):
     with open(file_kb_fquad, encoding='UTF-8') as f_kb:
         kb_fquad = json.load(f_kb)
 
@@ -134,12 +81,12 @@ def main(file_kb_fquad,file_test_fquad, name='fquad_eval'):
     kb_fquad = remove_answers_from_kb(kb_fquad)
     modified_fquad = merge(kb_fquad, test_fquad)
 
-    #todo: add a merging of the different paragraphs
+    # todo: add a merging of the different paragraphs
     save_dataset(modified_fquad, name)
+
 
 if __name__ == '__main__':
     file_kb_fquad = Path('./data/evaluation_datasets/fquad_train.json')
     file_test_fquad = Path('./data/evaluation_datasets/fquad_valid.json')
 
-    main(file_kb_fquad,file_test_fquad)
-
+    main(file_kb_fquad, file_test_fquad)
