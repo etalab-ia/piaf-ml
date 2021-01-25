c8ec0692bf4f56830b96806e42bf87a6705a64fb
Rob192 <reynaud.robin@gmail.com>
2021-01-25 17:25:40 +0100

    modify retriever_reader_eval_squad.py to save results

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -1,10 +1,13 @@
 parameters = {
-    "k_retriever": [1],
-    "k_reader": [1],
+    "k_retriever": [3,15],
+    "k_reader": [1,3,5],
     "retriever_type": ["bm25"],
-    "squad_dataset": ["./data/evaluation_datasets/fquad_eval.json"],
+    "squad_dataset": ["./data/evaluation-datasets/spf_squad.json"],
     "filter_level": [None],
-    "preprocessing": [True]
+    "preprocessing": [True],
+    "split_by": ["word"], #Can be "word", "sentence", or "passage"
+    "split_length":[100,200,500,1000],
+    "split_respect_sentence_boundary": [True]
 }
 # rules:
 # corpus and retriever type requires reloading ES indexing
--- src/evaluation/retriever_reader/retriever_reader_eval_squad.py
+++ src/evaluation/retriever_reader/retriever_reader_eval_squad.py
@@ -1,6 +1,8 @@
 import hashlib
 import logging
 import torch
+import socket
+from datetime import datetime
 
 from pathlib import Path
 from tqdm import tqdm
@@ -9,6 +11,7 @@ from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from haystack.retriever.sparse import ElasticsearchRetriever
 from haystack.retriever.dense import EmbeddingRetriever
 from haystack.reader.transformers import TransformersReader
+from haystack.preprocessor.preprocessor import PreProcessor
 from haystack.pipeline import Pipeline
 
 from farm.utils import initialize_device_settings
@@ -16,8 +19,7 @@ from sklearn.model_selection import ParameterGrid
 
 from src.evaluation.config.retriever_reader_eval_squad_config import parameters
 from src.evaluation.utils.elasticsearch_management import launch_ES, prepare_mapping
-from src.evaluation.utils.preprocess import add_eval_data_from_file
-from src.evaluation.utils.utils_eval import eval_retriever_reader
+from src.evaluation.utils.utils_eval import eval_retriever_reader, save_results
 from src.evaluation.config.elasticsearch_mappings import SQUAD_MAPPING
 
 GPU_AVAILABLE = torch.cuda.is_available()
@@ -42,52 +44,68 @@ def single_run(parameters):
     k_retriever = parameters["k_retriever"]
     k_reader = parameters["k_reader"]
     preprocessing = parameters["preprocessing"]
+    split_by = parameters["split_by"]
+    split_length = parameters["split_length"]
+    split_respect_sentence_boundary = parameters["split_respect_sentence_boundary"]
     experiment_id = hashlib.md5(str(parameters).encode("utf-8")).hexdigest()[:4]
     # Prepare framework
 
-    prepare_mapping(SQUAD_MAPPING, preprocessing)
+    p = Pipeline()
 
-    # Connect to Elasticsearch
-    document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document",
-                                                create_index=False, embedding_field="emb",
-                                                embedding_dim=512, excluded_meta_data=["emb"], similarity='cosine',
-                                                custom_mapping=SQUAD_MAPPING)
+    prepare_mapping(SQUAD_MAPPING, preprocessing, embedding_dimension=512)
 
-    # Initialize Retriever
-    retriever_bm25 = ElasticsearchRetriever(document_store=document_store)
-    retriever_emb = EmbeddingRetriever(document_store=document_store,
+    preprocessor = PreProcessor(
+        clean_empty_lines=False,
+        clean_whitespace=False,
+        clean_header_footer=False,
+        split_by=split_by,
+        split_length=split_length,
+        split_overlap=0, #this must be set to 0 at the data of writting this: 22 01 2021
+        split_respect_sentence_boundary=split_respect_sentence_boundary
+    )
+
+    if retriever_type == 'bm25':
+
+        document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document_xp",
+                                                    create_index=False, embedding_field="emb",
+                                                    embedding_dim=512, excluded_meta_data=["emb"], similarity='cosine',
+                                                    custom_mapping=SQUAD_MAPPING)
+        retriever = ElasticsearchRetriever(document_store=document_store)
+        p.add_node(component=retriever, name="ESRetriever", inputs=["Query"])
+
+    elif retriever_type == "sbert":
+        document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document_xp",
+                                                    create_index=False, embedding_field="emb",
+                                                    embedding_dim=512, excluded_meta_data=["emb"], similarity='cosine',
+                                                    custom_mapping=SQUAD_MAPPING)
+        retriever = EmbeddingRetriever(document_store=document_store,
                                        embedding_model="distiluse-base-multilingual-cased",
                                        use_gpu=GPU_AVAILABLE, model_format="sentence_transformers",
                                        pooling_strategy="reduce_max",
-                                       emb_extraction_layer=-2)
+                                       emb_extraction_layer=-1)
+        p.add_node(component=retriever, name="SBertRetriever", inputs=["Query"])
+    else:
+        raise Exception(f"You chose {retriever_type}. Choose one from bm25, sbert, or dpr")
+
 
     reader = TransformersReader(model_name_or_path="etalab-ia/camembert-base-squadFR-fquad-piaf",
                                 tokenizer="etalab-ia/camembert-base-squadFR-fquad-piaf",
                                 use_gpu=n_gpu,top_k_per_candidate=k_reader)
-
-    p = Pipeline()
-
-    if retriever_type == 'bm25':
-        retriever = retriever_bm25
-        p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    elif retriever_type == "sbert":
-        retriever = retriever_emb
-        p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    else:
-        raise Exception(f"You chose {retriever_type}. Choose one from bm25, sbert, or dpr")
 
     p.add_node(component=reader, name='reader', inputs=['Retriever'])
 
     # Add evaluation data to Elasticsearch document store
     # We first delete the custom tutorial indices to not have duplicate elements
     # make sure these indices do not collide with existing ones, the indices will be wiped clean before data is inserted
-    doc_index = "document"
-    label_index = "label"
+    doc_index = "document_xp"
+    label_index = "label_xp"
 
     document_store.delete_all_documents(index=doc_index)
     document_store.delete_all_documents(index=label_index)
     document_store.add_eval_data(evaluation_data.as_posix(), doc_index=doc_index, label_index=label_index)
-    document_store.update_embeddings(retriever_emb, index=doc_index)
+
+    if retriever_type in ["sbert", "dpr"]:
+        document_store.update_embeddings(retriever, index=doc_index)
 
     retriever_eval_results = eval_retriever_reader(document_store=document_store, pipeline=p, top_k_reader=k_reader,
                                                    top_k_retriever=k_retriever, label_index=label_index,
@@ -96,24 +114,26 @@ def single_run(parameters):
     print("Reader Accuracy:", retriever_eval_results["reader_topk_accuracy"])
     print("reader_topk_f1:", retriever_eval_results["reader_topk_f1"])
 
+    retriever_eval_results.update(parameters)
+    retriever_eval_results.update({"date": datetime.today().strftime('%Y-%m-%d_%H-%M-%S'),
+                                   "hostname": socket.gethostname(),
+                                   "experiment_id": experiment_id})
+
     return retriever_eval_results
 
 
 if __name__ == '__main__':
-    result_file_path = Path("./results/results.csv")
+    result_file_path = Path("./results/results_reader.csv")
     parameters_grid = list(ParameterGrid(param_grid=parameters))
 
-    device, n_gpu = initialize_device_settings(use_cuda=False)
-
-    logger = logging.getLogger(__name__)
+    device, n_gpu = initialize_device_settings(use_cuda=True)
+    GPU_AVAILABLE = 1 if device == "gpu" else 0
 
     all_results = []
     launch_ES()
     for param in tqdm(parameters_grid, desc="GridSearch"):
         # START XP
         run_results = single_run(param)
-        print(run_results)
         all_results.append(run_results)
 
-    #save_results(result_file_path=result_file_path, all_results=all_results)
-    print(all_results)
+    save_results(result_file_path=result_file_path, results_list=all_results)
