3483e69ce9eb737fc67006e0776259da853a16a2
Rob192 <reynaud.robin@gmail.com>
2021-01-29 11:12:54 +0100

    add some description of the data in folder evaluation-datasets

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -2,7 +2,7 @@ parameters = {
     "k_retriever": [3,15,30,45,60],
     "k_reader": [3,5,10],
     "retriever_type": ["bm25"],
-    "squad_dataset": ["./data/evaluation-datasets/spf_squad.json", "data/evaluation-datasets/fquad_eval.json"],
+    "squad_dataset": ["./data/evaluation-datasets/full_spf_squad.json", "data/evaluation-datasets/fquad_eval.json"],
     "filter_level": [None],
     "preprocessing": [True],
     "split_by": ["word"], #Can be "word", "sentence", or "passage"
