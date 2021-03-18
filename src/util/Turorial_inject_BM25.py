80c0eae56e223e4afb8af47cb4525b84ab426bc2
Guillim <guillim@users.noreply.github.com>
2021-03-18 16:11:49 +0100

    adding SQuAD import option
    We noticed deepset.ai had developped a way to convert a SQuAD like JSON  format. So we use their converter called `eval_data_from_json` to create the dicts to be injected into ElasticSearch. Beauty of it : all the meta data are stored as well.

--- src/util/Turorial_inject_BM25.py
+++ src/util/Turorial_inject_BM25.py
@@ -84,48 +84,54 @@ MAPPING = {
 
 document_store = ElasticsearchDocumentStore(host="haystack_elasticsearch_1", username="", password="", index="document_elasticsearch", custom_mapping=MAPPING)
 
+# New way of getting docs from SQuAD like format
+from haystack.preprocessor.utils import eval_data_from_json
 
-def get_arbo(dict, level):
-    if "arborescence" in dict:
-        try:
-            return dict["arborescence"][level]
-        except:  # in case the level does not exist or there is no dict at all
-            return ''
-
-
-def convert_json_files_to_dicts(dir_path: str):
-    file_paths = [p for p in Path(dir_path).glob("**/*")]
-    documents = []
-    
-    for path in tqdm(file_paths):
-        if path.suffix.lower() == ".json":
-            with open(path) as doc:
-                json_doc = json.load(doc)
-
-            text = json_doc["text"]
-            embedding = []
-            audience = get_arbo(json_doc, 'audience')
-            theme = get_arbo(json_doc, 'theme')
-            sous_theme = get_arbo(json_doc, 'sous_theme')
-            dossier = get_arbo(json_doc, 'dossier')
-            sous_dossier = get_arbo(json_doc, 'sous_dossier')
-        else:
-            raise Exception(f"Indexing of {path.suffix} files is not currently supported.")
-
-        text_reader = json_doc["text_reader"] if "text_reader" in json_doc else text
-        # we have to remove "embedding": embedding from the JSON otherwise if will raise an error
-        documents.append({"text": text_reader,
-                          "question_sparse": text,
-                          "meta": {"name": path.name,
-                                   "link": f"https://www.service-public.fr/particuliers/vosdroits/{path.name.split('--', 1)[0]}",
-                                   "audience": audience,
-                                   "theme": theme,
-                                   "sous_theme": sous_theme,
-                                   "dossier": dossier,
-                                   "sous_dossier": sous_dossier}})
-    return documents
-    
-    
-
-dicts = convert_json_files_to_dicts(dir_path="data/v14")
+dicts, labels = eval_data_from_json("data/squad_lie_formated.json")
 document_store.write_documents(dicts)
+
+# Old way - will soon be deleted (as of mar 2021, making the change) - for folders filled with .json files
+# def get_arbo(dict, level):
+#     if "arborescence" in dict:
+#         try:
+#             return dict["arborescence"][level]
+#         except:  # in case the level does not exist or there is no dict at all
+#             return ''
+
+
+# def convert_json_files_to_dicts(dir_path: str):
+#     file_paths = [p for p in Path(dir_path).glob("**/*")]
+#     documents = []
+    
+#     for path in tqdm(file_paths):
+#         if path.suffix.lower() == ".json":
+#             with open(path) as doc:
+#                 json_doc = json.load(doc)
+
+#             text = json_doc["text"]
+#             embedding = []
+#             audience = get_arbo(json_doc, 'audience')
+#             theme = get_arbo(json_doc, 'theme')
+#             sous_theme = get_arbo(json_doc, 'sous_theme')
+#             dossier = get_arbo(json_doc, 'dossier')
+#             sous_dossier = get_arbo(json_doc, 'sous_dossier')
+#         else:
+#             raise Exception(f"Indexing of {path.suffix} files is not currently supported.")
+
+#         text_reader = json_doc["text_reader"] if "text_reader" in json_doc else text
+#         # we have to remove "embedding": embedding from the JSON otherwise if will raise an error
+#         documents.append({"text": text_reader,
+#                           "question_sparse": text,
+#                           "meta": {"name": path.name,
+#                                    "link": f"https://www.service-public.fr/particuliers/vosdroits/{path.name.split('--', 1)[0]}",
+#                                    "audience": audience,
+#                                    "theme": theme,
+#                                    "sous_theme": sous_theme,
+#                                    "dossier": dossier,
+#                                    "sous_dossier": sous_dossier}})
+#     return documents
+    
+    
+
+# dicts = convert_json_files_to_dicts(dir_path="data/v14")
+# document_store.write_documents(dicts)
