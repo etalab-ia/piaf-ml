ecb9defa59856a3c27f1df8b750771793da4e155
Rob192 <reynaud.robin@gmail.com>
2021-03-29 15:04:12 +0200

    Add comments

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -1,10 +1,10 @@
 parameters = {
     "k_retriever": [3],
-    "k_title_retriever" : [3], # must be present but only used when retriever_type == title_bm25
+    "k_title_retriever" : [3], # must be present, but only used when retriever_type == title_bm25
     "k_reader_per_candidate": [1],
     "k_reader_total": [1],
     "retriever_type": ["title_bm25"], # Can be bm25, sbert, dpr or title_bm25
-    "squad_dataset": ["./test/samples/squad/tiny.json"],
+    "squad_dataset": ["./test/samples/squad/small.json"],
     "filter_level": [None],
     "preprocessing": [True],
     "boosting" : [1], #default to 1
