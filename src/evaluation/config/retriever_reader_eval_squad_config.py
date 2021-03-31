248e9ef37d10d147133cf93b16ccc54f35850c30
Rob192 <reynaud.robin@gmail.com>
2021-03-31 12:16:17 +0200

    Revert "Change full_spf_squad to add missing titles to documents associated with dossiers"
    This reverts commit e70dc7fa3dfdbfdac498238dd1a948b5d8da094f.

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -1,10 +1,10 @@
 parameters = {
-    "k_retriever": [20,30],
-    "k_title_retriever" : [10], # must be present, but only used when retriever_type == title_bm25
-    "k_reader_per_candidate": [5],
-    "k_reader_total": [3],
-    "retriever_type": ["bm25","title_bm25"], # Can be bm25, sbert, dpr or title_bm25
-    "squad_dataset": ["./data/evaluation-datasets/full_spf_squad.json"],
+    "k_retriever": [3],
+    "k_title_retriever" : [3], # must be present, but only used when retriever_type == title_bm25
+    "k_reader_per_candidate": [1],
+    "k_reader_total": [1],
+    "retriever_type": ["title_bm25"], # Can be bm25, sbert, dpr or title_bm25
+    "squad_dataset": ["./test/samples/squad/small.json"],
     "filter_level": [None],
     "preprocessing": [True],
     "boosting" : [1], #default to 1
