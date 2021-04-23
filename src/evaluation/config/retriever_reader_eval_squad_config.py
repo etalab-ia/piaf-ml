0a4b0d9e3925ceb9ee6adb9d932576a58e0e66da
Rob192 <reynaud.robin@gmail.com>
2021-04-23 11:57:06 +0200

    First modification of full_spf_squad.json to remove missing id errors

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -3,8 +3,8 @@ parameters = {
     "k_title_retriever" : [10], # must be present, but only used when retriever_type == title_bm25
     "k_reader_per_candidate": [5],
     "k_reader_total": [3],
-    "retriever_type": ["title"], # Can be bm25, sbert, dpr, title or title_bm25
-    "squad_dataset": ["/home/pavel/code/piaf-ml/test/samples/squad/tiny.json"],
+    "retriever_type": ["bm25"], # Can be bm25, sbert, dpr, title or title_bm25
+    "squad_dataset": ["./data/evaluation-datasets/full_spf_squad.json"],
     "filter_level": [None],
     "preprocessing": [True],
     "boosting" : [1], #default to 1
