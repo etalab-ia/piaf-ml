9da6c0f44c735f91df8f3d65b751f19120d8bf6e
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-05-18 17:42:14 +0200

    removed dpr model from config for now

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -3,7 +3,6 @@ parameters = {
     "k_title_retriever" : [1], # must be present, but only used when retriever_type == title_bm25
     "k_reader_per_candidate": [20],
     "k_reader_total": [3],
-    "reader_model_version": ["053b085d851196110d7a83d8e0f077d0a18470be"],
     "retriever_model_version": ["1a01b38498875d45f69b2a6721bf6fe87425da39"],
     "dpr_model_version": [],
     "retriever_type": ["sbert"], # Can be bm25, sbert, dpr, title or title_bm25
