97e232356e5780fd9056bd999f5e7d59f16786c4
Rob192 <reynaud.robin@gmail.com>
2021-05-19 16:35:48 +0200

    First version of the google_retriever.py used for baseline of final users' expectations

---
+++ src/evaluation/utils/google_retriever.py
@@ -0,0 +1,116 @@
+import logging
+from typing import Optional, List
+
+from googlesearch import search
+from haystack import Document
+
+from haystack.retriever.base import BaseRetriever
+from haystack.document_store.base import BaseDocumentStore
+
+logger = logging.getLogger(__name__)
+
+
+class GoogleRetriever(BaseRetriever):
+
+    def __init__(self,
+                 document_store: BaseDocumentStore,
+                 top_k: int = 10,
+                 website: str = "service-public.fr"):
+        """
+            The GoogleRetriever is intended for establishing a baseline of the final clients' expectations.
+
+            :param document_store: The documentstore that is used for retrieving documents
+            :param top_k: How many documents to retrieve
+            :param website: The website in which google has to find results. The url retrieved by google will then be search inside the document_store
+        """
+
+        self.document_store = document_store
+        self.top_k = top_k
+        self.website = website
+
+
+def retrieve(self, query: str, filters: dict = None, top_k: Optional[int] = None, index: str = None) -> List[
+    Document]:
+    """
+        Scan through documents in DocumentStore and return a small number documents
+        that are most relevant to the query.
+
+        :param query: The query
+        :param filters: A dictionary where the keys specify a metadata field and the value is a list of accepted values for that field
+        :param top_k: How many documents to return per query.
+        :param index: The name of the index in the DocumentStore from which to retrieve documents
+    """
+
+    if top_k is None:
+        top_k = self.top_k
+    if not self.document_store:
+        logger.error(
+            "Cannot perform retrieve() since DensePassageRetriever initialized with document_store=None")
+        return []
+    if index is None:
+        index = self.document_store.index
+
+    query_website = f"site:{self.website} " + query  # add website restriction to query
+
+    gsearch_results = [url for url in search(query_website, tld="fr", num=top_k, stop=top_k,
+                                             pause=2)]  # the list of plain url retrieved by google
+
+    documents = []
+    for g in gsearch_results:
+        document_list = document_store.query("*", filters={"link": [g]})
+        if len(document_list) > 0:
+            documents.append(document_list[0])
+
+        return documents
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
+    from src.evaluation.config.elasticsearch_mappings import SQUAD_MAPPING
+    from src.evaluation.utils.elasticsearch_management import delete_indices, launch_ES
+
+    launch_ES()
+
+    # indexes for the elastic search
+    doc_index = "document_xp"
+    label_index = "label_xp"
+
+    # deleted indice for elastic search to make sure mappings are properly passed
+    delete_indices(index=doc_index)
+    delete_indices(index=label_index)
+
+    document_store = ElasticsearchDocumentStore(
+        host="localhost",
+        username="",
+        password="",
+        index=doc_index,
+        search_fields=["name", "text"],
+        create_index=False,
+        embedding_field="emb",
+        scheme="",
+        embedding_dim=768,
+        excluded_meta_data=["emb"],
+        similarity="cosine",
+        custom_mapping=SQUAD_MAPPING,
+    )
+
+    # Add evaluation data to Elasticsearch document store
+    evaluation_data = Path("./clients/dila/knowledge_base/squad.json")
+    document_store.add_eval_data(
+        evaluation_data.as_posix(),
+        doc_index=doc_index,
+        label_index=label_index,
+    )
+
+    website = 'service-public.fr'
+    query = "comment refaire ma carte d'identité"
+    query_website = f"site:{website} " + query
+
+    gsearch_results = [url for url in search(query_website, tld="fr", num=10, stop=10, pause=2)]
+    gsearch_results = []
+    for j in search(query, tld="fr", num=10, stop=10, pause=2):
+        gsearch_results.append(j)
+    documents = []
+    for g in gsearch_results:
+        documents.append(document_store.query("*", filters={"link": [g]}))
