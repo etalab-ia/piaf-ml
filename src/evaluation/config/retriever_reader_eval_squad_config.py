5b0798324799947e759425b837d7404323715299
Rob192 <reynaud.robin@gmail.com>
2021-05-27 13:14:33 +0200

    Params for DILA_fullspfV1 xp

--- src/evaluation/config/retriever_reader_eval_squad_config.py
+++ src/evaluation/config/retriever_reader_eval_squad_config.py
@@ -1,24 +1,18 @@
 parameters = {
     "k_retriever": [1],
-    "k_title_retriever": [
-        10
-    ],  # must be present, but only used when retriever_type == title_bm25
-    "k_reader_per_candidate": [1],
-    "k_reader_total": [1],
+    "k_title_retriever" : [1], # must be present, but only used when retriever_type == title_bm25
+    "k_reader_per_candidate": [20],
+    "k_reader_total": [5],
     "reader_model_version": ["053b085d851196110d7a83d8e0f077d0a18470be"],
     "retriever_model_version": ["1a01b38498875d45f69b2a6721bf6fe87425da39"],
-    "retriever_type": [
-        "sbert",
-        "title_bm25",
-        "bm25",
-    ],  # Can be bm25, sbert, dpr, title or title_bm25
-    "squad_dataset": ["./clients/cnil/knowledge_base/squad.json"],
+    "retriever_type": ["bm25", "title_bm25"], # Can be bm25, sbert, dpr, title or title_bm25
+    "squad_dataset": ["./clients/dila/knowledge_base/squad.json"],
     "filter_level": [None],
     "preprocessing": [False],
-    "boosting": [1, 10],  # default to 1
+    "boosting" : [1], #default to 1
     "split_by": ["word"],  # Can be "word", "sentence", or "passage"
-    "split_length": [1000000],
-    "experiment_name": ["dev"],
+    "split_length": [1000],
+    "experiment_name": ["DILA_fullspfV1"]
 }
 # rules:
 # corpus and retriever type requires reloading ES indexing
