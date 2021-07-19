fe28cc99b9dbe77fe81f2c1c881876bb531493dd
Guillaume Chérel <guillaume.cherel@cosimus.com>
2021-07-19 17:42:18 +0200

    Add explanatory comment about epitca_perf_file in config file.

--- src/evaluation/config/retriever_eval_squad_config.py
+++ src/evaluation/config/retriever_eval_squad_config.py
@@ -5,6 +5,8 @@ parameters = {
     "squad_dataset": [
         "./clients/cnil/knowledge_base/squad.json"
     ],  # data/evaluation-datasets/fquad_valid_with_impossible_fraction.json data/evaluation-datasets/testing_squad_format.json
+    # Path to the Epitca performance file or None. Needed when using the
+    # retriever_type 'epitca'.
     "epitca_perf_file": ["./clients/cnil/knowledge_base/raw_and_preparation/epitca_perf_V2.json"],
     "filter_level": [None],
     "boosting": [1],
