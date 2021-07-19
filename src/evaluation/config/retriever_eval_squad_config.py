856a4252c6f9010348fc24ea7289756d51f304df
Guillaume Chérel <guillaume.cherel@cosimus.com>
2021-07-19 16:32:22 +0200

    Configure piaf vs epitca run.

--- src/evaluation/config/retriever_eval_squad_config.py
+++ src/evaluation/config/retriever_eval_squad_config.py
@@ -1,10 +1,11 @@
 parameters = {
-    "k": [3],
-    "retriever_type": ['bm25'],
+    "k": [1, 3],
+    "retriever_type": ['epitca', 'bm25'],
     "google_retriever_website": ['service-public.fr'],
     "squad_dataset": [
         "./clients/cnil/knowledge_base/squad.json"
     ],  # data/evaluation-datasets/fquad_valid_with_impossible_fraction.json data/evaluation-datasets/testing_squad_format.json
+    "epitca_perf_file": ["./clients/cnil/knowledge_base/raw_and_preparation/epitca_perf_V2.json"],
     "filter_level": [None],
     "boosting": [1],
     "preprocessing": [False],
